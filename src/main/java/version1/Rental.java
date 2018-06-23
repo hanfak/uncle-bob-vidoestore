02bfd00ae2123b34a3db4eb5d38d38b41ff10a6d
Hanif Fakira <fakira.work@gmail.com>
2018-06-23 07:19:01 +0100

    move amountFor to Rental, and renamed

--- src/main/java/version1/Rental.java
+++ src/main/java/version1/Rental.java
@@ -6,6 +6,27 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    public double amountFor(Rental aRental) {
+        double thisAmount = 0;
+
+        switch (aRental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (aRental.getDaysRented() > 2)
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += aRental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (aRental.getDaysRented() > 3)
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
     public int getDaysRented() {
         return daysRented;
     }
