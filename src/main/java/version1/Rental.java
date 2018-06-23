b95b7a15a0fc3b2478a171b212c240960aef5368
Hanif Fakira <fakira.work@gmail.com>
2018-06-23 07:46:27 +0100

    move getCharge to Movie, as passing in Movie object is prone to change, and additions of movies should remain in movie class and associated additions to methods kept in smae class to avoid changing more classes

--- src/main/java/version1/Rental.java
+++ src/main/java/version1/Rental.java
@@ -1,6 +1,9 @@
 package version1;
 
 public class Rental {
+    private Movie movie;
+    private int daysRented;
+
     public Rental(Movie movie, int daysRented) {
         this.movie = movie;
         this.daysRented = daysRented;
@@ -14,27 +17,6 @@ public class Rental {
         }
     }
 
-    public double getCharge() {
-        double thisAmount = 0;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
     public int getDaysRented() {
         return daysRented;
     }
@@ -43,6 +25,7 @@ public class Rental {
         return movie;
     }
 
-    private Movie movie;
-    private int daysRented;
+    public double getCharge() {
+        return movie.getCharge(getDaysRented());
+    }
 }
