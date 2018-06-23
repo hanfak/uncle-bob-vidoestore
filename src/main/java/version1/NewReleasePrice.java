c1da154477780f8fef3f18c0e1994bf47c08fd54
Hanif Fakira <fakira.work@gmail.com>
2018-06-23 14:30:22 +0100

    Move frequentRenterPoints logic to NewReleasePrice

--- src/main/java/version1/NewReleasePrice.java
+++ src/main/java/version1/NewReleasePrice.java
@@ -10,4 +10,9 @@ class NewReleasePrice extends Price {
     public double getAmount(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        return daysRented > 1 ? 2 : 1;
+    }
 }
--- src/main/java/version1/Price.java
+++ src/main/java/version1/Price.java
@@ -2,13 +2,10 @@ package version1;
 
 abstract class Price {
     abstract int getPriceCode();
+
     abstract double getAmount(int daysRented);
 
     int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return 1;
     }
 }
