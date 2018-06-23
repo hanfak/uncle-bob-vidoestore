1036dab2cf3c1300d328a9565fe148f67aa41d10
Hanif Fakira <fakira.work@gmail.com>
2018-06-23 07:12:11 +0100

    Use arraylist instead of vector data structure, use for loop instead of iterator, renamed each to rental

--- src/main/java/version1/Customer.java
+++ src/main/java/version1/Customer.java
@@ -1,18 +1,18 @@
 package version1;
 
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
     private final String name;
-    private final Vector rentals = new Vector();
+    private final List<Rental> rentals = new ArrayList<>();
 
     public Customer(String name) {
         this.name = name;
     }
 
     public void addRental(Rental rental) {
-        rentals.addElement(rental);
+        rentals.add(rental);
     }
 
     public String getName() {
@@ -22,26 +22,23 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
+
         String result = "Rental Record for " + getName() + "\n";
 
-        while (rentals.hasMoreElements()) {
+        for (Rental rental :rentals) {
             double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
 
-            // determines the amount for each line
-            thisAmount = amountFor(each);
+            thisAmount = amountFor(rental);
 
             frequentRenterPoints++;
 
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                    && each.getDaysRented() > 1)
+            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                    && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
 
-            result += "\t" + each.getMovie().getTitle() + "\t"
+            result += "\t" + rental.getMovie().getTitle() + "\t"
                     + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
-
         }
 
         result += "You owed " + String.valueOf(totalAmount) + "\n";
@@ -51,21 +48,21 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental rental) {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
         }
         return thisAmount;
