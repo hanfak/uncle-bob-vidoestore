19fb33d762566b6e04b9ba156e6601a65942af49
Hanif Fakira <fakira.work@gmail.com>
2018-06-23 07:05:33 +0100

    Reorder elements in class

--- src/main/java/version1/Customer.java
+++ src/main/java/version1/Customer.java
@@ -4,6 +4,9 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
+    private final String name;
+    private final Vector rentals = new Vector();
+
     public Customer(String name) {
         this.name = name;
     }
@@ -68,7 +71,4 @@ public class Customer {
         return thisAmount;
     }
 
-
-    private String name;
-    private Vector rentals = new Vector();
 }
